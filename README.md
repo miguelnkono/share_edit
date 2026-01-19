5d52a3346af93a4b9a912814d5a8449827ac70d2
miguelnkono <nkonondememiguel7@gmail.com>
2026-01-19 13:49:24 +0100

    putted the minimal server in place

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
+### SHARE EDIT 
+
+Share_edit is a tool that allow the sharing of ressource locally while working in team.
 
--- main.go
+++ main.go
@@ -1,7 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"strings"
+)
 
 func main() {
-	fmt.Println("hello, world!")
+	// listener for new connections
+	listener, err := net.Listen("tcp", "localhost:8080")
+	if err != nil {
+		log.Fatal("Error listening:", err)
+	}
+	defer listener.Close()
+
+	// accept new connections
+	for {
+		connection, err := listener.Accept()
+		if err != nil {
+			log.Fatal("Error while waiting for new connections: ", err)
+			continue
+		}
+
+		// handle each connection in a new goroutine
+        fmt.Printf("%s connected\n", connection.LocalAddr().String())
+		go handle_connection(connection)
+	}
+}
+
+func handle_connection(connection net.Conn) {
+	defer connection.Close()
+
+	reader := bufio.NewReader(connection)
+	message, err := reader.ReadString('\n')
+	if err != nil {
+		log.Fatal("Error while reading data: %v", err)
+		return
+	}
+
+	message_to_send := strings.ToUpper(strings.TrimSpace(message))
+	response := fmt.Sprintf("ACK: %s\n", message_to_send)
+	_, err = connection.Write([]byte(response))
+	if err != nil {
+		log.Fatal("Error while sending the response to the client: %v", err)
+	}
 }
